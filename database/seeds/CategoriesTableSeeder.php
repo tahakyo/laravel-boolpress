fcc1af87071808ff9aca3106f7cc682a9b7ca3e4
tahakyo <ghazouani1988@email.com>
2022-07-10 19:23:52 +0200

    add categories table and seeder on db

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [
+            'primi piatti',
+            'secondo piatti',
+            'antipasti',
+            'contorni',
+            'dessert'
+        ];
+
+        foreach ($categories as $category) {
+            $new_category = new Category();
+            $new_category->name = $category;
+            $new_category->slug = Str::slug($category, '-');
+            $new_category->save();
+        }
+    }
+}
