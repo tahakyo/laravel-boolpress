4aa1a25768e0ab3d832ea6512816e243156e1d66
tahakyo <ghazouani1988@email.com>
2022-07-10 02:24:10 +0200

    add userInfo hasOne function

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -10,6 +10,8 @@ class HomeController extends Controller
 {
     public function index() {
         $user = Auth::user();
+        // $userInfo = $user->userInfo;
+        // dd($userInfo);
         return view('admin.home', compact('user'));
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,4 +3,6 @@
 @section('content')
     <h1>Hello {{ $user->name }} welcome to</h1>
     <h1>Back-Office Homepage</h1>
+    <p>Adress: {{ $user->userInfo->adress }}</p>
+    <p>Phone: {{ $user->userInfo->phone }}</p>
 @endsection
